92033e1a8acd38a74dd805239d5a99766ede19e3
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-10-05 14:18:24 -0300

    testes

--- cypress/e2e/testes.cy.js
+++ cypress/e2e/testes.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/* eslint-disable no-undef */
+
+describe("Teste de abertura do sistema", () => {
+  it("Deve abrir o sistema sem erros", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get(".logo").should("exist");
+  });
+});
+
+describe("Teste de navegação ao escolher um time", () => {
+  it("Deve navegar para a próxima página ao clicar em um time", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get(".li-link:eq(1)").should("exist").click();
   })
 })
